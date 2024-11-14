3e93fd49ff0e637db8a917932bdf80ff24f390d8
yewbdar <yewbdar.grima@gmail.com>
2024-11-14 17:28:15 -0600

    to do list functionlity added

--- to-do-list-react/src/App.js
+++ to-do-list-react/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [arrayOfTodo, setArrayOfTodo] = useState(["go to charchs"]);
+  const [inputValue, setInputValue] = useState();
+
+  const onSubmitCliked = (e) => {
+    e.preventDefault();
+    setArrayOfTodo([...arrayOfTodo, inputValue]);
+  };
+
+  const deleteItem = (item) => {
+    setArrayOfTodo(arrayOfTodo.filter((items) => items !== item));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Simple To-Do List</h1>
+
+      <form id="todo-form">
+        <input
+          type="text"
+          id="todo-input"
+          placeholder="Add a new task"
+          required
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="submit" onClick={onSubmitCliked}>
+          Add Task
+        </button>
+      </form>
+      <ul id="todo-list">
+        {arrayOfTodo?.map((item, index) => {
+          return (
+            <li key={index}>
+              {item}
+              <button key={item} type="button" onClick={() => deleteItem(item)}>
+                delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
